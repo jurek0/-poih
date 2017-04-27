dbba237557d24629be824cc784930c00db486d82
jurek0 <jurekpfaff@gmail.com>
2017-04-27 13:42:20 +0200

    Update Fenster.java

--- Source-Code/abc/Fenster/Fenster.java
+++ Source-Code/abc/Fenster/Fenster.java
@@ -4,3 +4,17 @@ import Source-Code.Rechner;
 import Source-Code.Oberflaeche;
 
 import javax.swing.*;
+import java.awt.event.ActionListener; 
+import java.awt.event.ActionEvent;
+
+public class Fenster{
+  Rechner rechner = new Rechner();
+  Oberflaeche rechnerOberflaeche = new Oberflaeche();
+  calc Calc = new calc();
+  
+ public void start (){
+   Fenster fenster = new Fenster();
+   fenster.setVisible (true);
+ }
+  
+}
