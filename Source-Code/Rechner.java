e8fa508912678ddb2e625400fd4247c83cb6826c
jurek0 <jurekpfaff@gmail.com>
2017-04-27 15:52:41 +0200

    Update Rechner.java

--- Source-Code/Rechner.java
+++ Source-Code/Rechner.java
@@ -6,27 +6,32 @@ import javax.swing.JButton;
  
 public class Rechner {
 		
+ /*
+ * 		Diese Methode Speichert das Eingegebene in einer Linked List, aus der
+ * 		sie die Rechenmethode Calc verarbeiten kann. 
+ */
+	
 //		Die Oberfläche Des Rechners 
-// Oberflaeche rechnerOberflaeche = new Oberflaeche();
+// 		Oberflaeche rechnerOberflaeche = new Oberflaeche();
 	
 //		Die Eigentlichen Rechen-Operationen 
 //		calc Calc = new calc ();
 
-//		Umgang Mit Klammern
-//		Klammern rechnerKlammern = new Klammern();
 		
 		LinkedList<String> Eingabe = new LinkedList<>(); 
-//		Das Entgültige Ergebnis 
-		String Ergebnis = "";
-//		Eingabe String 
+
+		String Ergebnis = ""; //Das Entgültige Ergebnis 
+
 		String [] abc;
 		
-		boolean isGRAD;
 		
-		public void rechnen (boolean isGRAD, String lösen){
+		
+		public void rechnen (String lösen){
+			// Tauscht Eingaben. Vor und nach die Operatoren kommt ein Leerzeichen
 			lösen = lösen.replace(",", " . ");
 			lösen = lösen.replace ("\u221A"," wur ");
 			lösen = lösen.replace("\u03c0", " pi ");
+			lösen = lösen.replace("e", " e ");
 			lösen = lösen.replace ("+"," + ");
 			lösen = lösen.replace ("-"," - ");
 			lösen = lösen.replace ("*"," * ");
@@ -35,30 +40,35 @@ public class Rechner {
 			lösen = lösen.replace ("cos("," cos( ");
 			lösen = lösen.replace ("tan("," tan( ");
 			lösen = lösen.replace("%", " % ");
+			lösen = lösen.replace("ln(", " ln( ");
+			lösen = lösen.replace("log(", " log( ");
+			lösen = lösen.replace("!", " fac ");
+			lösen = lösen.replace("sin^-1(", " sin^-1( ");
+			lösen = lösen.replace("cos^-1(", " cos^-1( ");
+			lösen = lösen.replace("tan^-1(", " tan^-1( ");
 			
-			abc = lösen.split(" ");
+			abc = lösen.split(" "); // Die Eingabe wird beim Leerzeichen gesplitten.
 			
 			//Eingegebene Elemente werden in den Array gepackt
 			for (int i = 0; i < abc.length; i++) {
 				Eingabe.add(abc[i]);
 				
 			
-	     	
+	     	Ergebnis = Eingabe.toString();
 			Ergebnis = Ergebnis.substring(1, Ergebnis.length()-1);
-			Ergebnis = Eingabe.toString();
+			
 		   Eingabe.clear();
 	}
-	
+	 
 			 
 			 
 		}
 		public String getErgebnis(){
 			return Ergebnis;
 		}
-		//Main Methode:   
-//	    public static void main(String[] args) {
-//	       new Rechner ();
+//Main Methode:   
+	    public static void main(String[] args) {
+	       new Rechner ();
 	       
-//	    }
+	    }
 }
-
